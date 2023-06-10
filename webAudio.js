3c46e9d2d348a100cf18f89d68312a7999d4443f
flightcoda <jonnyfox.contact@gmail.com>
2023-06-10 17:19:39 +0100

    yes

--- webAudio.js
+++ webAudio.js
@@ -2,8 +2,8 @@
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 // Load an audio file
-const audioElement = new Audio('media/ut-audio/paperloop-test.wav');
-const sourceNode = audioContext.createMediaElementSource(audioElement);
+const audioElement2 = new Audio('media/ut-audio/paperloop-test.wav');
+const sourceNode = audioContext.createMediaElementSource(audioElement2);
 
 // Select the play button element
 const playButton = document.querySelector('.play-button');
@@ -14,5 +14,5 @@ sourceNode.connect(audioContext.destination);
 // Attach an event listener to the play button
 playButton.addEventListener('click', function() {
     // Play the audio
-    audioElement.play();
+    audioElement2.play();
   });
